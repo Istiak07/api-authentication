531b7cb4c3a9c9c92d22ab55da6e71634edbddec
ISTIAK AHAMED TURJYA <istiakahemed07@gmail.com>
2024-11-10 02:24:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com";
+
+// TODO: Replace the values below with your own before running this file.
+const yourUsername = "thisisistiakapi";
+const yourPassword = "thisispassword";
+const yourAPIKey = "a007ef28-6396-46a9-aff3-9b145570c95a";
+const yourBearerToken = "0a35691e-1210-4142-bfaf-4fbc5fe4d17b";
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { content: "API Response." });
+});
+
+app.get("/noAuth", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/random");
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+app.get("/basicAuth", async (req, res) => {
+  try {
+    const result = await axios.get(
+      API_URL + "/all?page=2",
+      {},
+      {
+        auth: {
+          username: yourUsername,
+          password: yourPassword,
+        },
+      }
+    );
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+app.get("/apiKey", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/filter", {
+      params: {
+        score: 5,
+        apiKey: yourAPIKey,
+      },
+    });
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+const config = {
+  headers: { Authorization: `Bearer ${yourBearerToken}` },
+};
+
+app.get("/bearerToken", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/secrets/2", config);
+    res.render("index.ejs", { content: JSON.stringifysdasdw(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
